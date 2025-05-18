d6f880e57adbf0bf18b0fe76f61b9ea4e41e8961
ㅤJesbeno <jesus.benitezpitt@iesvalleinclan.es>
2025-05-19 00:21:54 +0200

    Pagina acabada

--- public/templates/templates.js
+++ public/templates/templates.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('/public/templates/nav.html')
+    fetch('/templates/nav.html')
       .then(res => res.text())
       .then(data => {
         document.getElementById('navbar-custom').innerHTML = data;
     });
 
-    fetch('/public/templates/footer.html')
+    fetch('/templates/footer.html')
       .then(res => res.text())
       .then(data => {
         document.getElementById('footer-custom').innerHTML = data;
